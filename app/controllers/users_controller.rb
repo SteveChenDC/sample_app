93084bd88b30f25ccc6d4cefc013d8f49e0440eb
Phillip <uzerifth@gmail.com>
2016-06-20 22:39:14 -0400

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @test = User.new
   end
 
   def show
     @user = User.find_by(params[:id])
+    @test = User.new
   end
 
   def create
     @user = User.new(user_params)
+    @test = User.new
     if @user.save
+      log_in @user
       flash[:success] = "welcome to the Sample App!"
       redirect_to @user
     else
