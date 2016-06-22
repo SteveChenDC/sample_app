542949ea392695261772e17413cfdc58aaf30cb9
SteveC <stevechenweb@gmail.com>
2016-06-21 21:09:39 -0400

    Add email validation to user.rb and email format regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
-
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                  format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 
