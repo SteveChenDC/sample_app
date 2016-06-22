778e7069eb5b5bfdef3e3e16c72662229259735d
SteveC <stevechenweb@gmail.com>
2016-06-21 21:12:10 -0400

    Add password validation to user.rb and format regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                  format: { with: VALID_EMAIL_REGEX },
-                  uniqueness: { case_sensitive: false }
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 8 },
+                       format: ((.*\d)(.*[A-Z]))
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 
